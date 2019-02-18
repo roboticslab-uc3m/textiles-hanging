9cd5442fc373f293564579c322387c0dd9e45502
David-Estevez <david.estevez.fdez@gmail.com>
2019-02-18 01:20:43 +0100

    Add training code (unfinished)

--- textiles_hanging/train_model.py
+++ textiles_hanging/train_model.py
@@ -2,14 +2,14 @@ from keras import backend as K
 from keras.models import Sequential
 from keras.layers.core import Flatten, Dense, Dropout
 from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
-from keras.optimizers import SGD
+from keras.optimizers import SGD, RMSprop, Adam
 import cv2, numpy as np
 
-# define a VGG16 network
 
+# define a VGG16 network
 def VGG_16(weights_path=None):
     model = Sequential()
-    model.add(ZeroPadding2D((1,1),input_shape=(1,224,224))) # 3, 224, 224
+    model.add(ZeroPadding2D((1,1),input_shape=(1,320,240)))  # 3, 224, 224  # Don't know yet if this should be squared
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(ZeroPadding2D((1,1)))
     model.add(Conv2D(64, (3, 3), activation='relu'))
@@ -48,23 +48,62 @@ def VGG_16(weights_path=None):
     model.add(Flatten())
 
     #top layer of the VGG net
-    model.add(Dense(4096, activation='relu'))
+    model.add(Dense(4096, activation='relu'))  # Should I remove these layers too?
     model.add(Dropout(0.5))
     model.add(Dense(4096, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(1000, activation='softmax'))
+    model.add(Dense(3, activation='linear'))
 
     if weights_path:
         model.load_weights(weights_path)
 
     return model
 
+
 if __name__ == "__main__":
+    # network and training
+    NB_EPOCH = 20
+    BATCH_SIZE = 128
+    VERBOSE = 1
+    OPTIMIZER = Adam()
+    VALIDATION_SPLIT = 0.2
+
+    IMG_ROWS, IMG_COLS = 28, 28  # input image dimensions
+    NB_CLASSES = 10  # number of outputs = number of digits
+    INPUT_SHAPE = (1, IMG_ROWS, IMG_COLS)
+
+    # data: shuffled and split between train and test sets
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()  # replace by our dataset
     K.set_image_dim_ordering("th")
 
     # Test pretrained model
     model = VGG_16()
-    optimizer = SGD()
 
     # ToDo: Add training code
+    model.compile(loss="mse", optimizer=OPTIMIZER, metrics=["accuracy", "mse"])
 
+    history = model.fit(X_train, y_train, batch_size=BATCH_SIZE, epochs=NB_EPOCH,
+                        verbose=VERBOSE, validation_split=VALIDATION_SPLIT)
+
+    score = model.evaluate(X_test, y_test, verbose=VERBOSE)
+    print("\nTest score:", score[0])
+    print('Test accuracy:', score[1])
+
+    # list all data in history
+    print(history.history.keys())
+    # summarize history for accuracy
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
