d25a3478ab547e638463997702e671fe56413970
David-Estevez <david.estevez.fdez@gmail.com>
2019-02-20 18:40:53 +0100

    Change stuff for testing (day 1)

--- textiles_hanging/train_model.py
+++ textiles_hanging/train_model.py
@@ -1,13 +1,17 @@
 import logging
 import os
+import pickle
 
 import begin
 import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from keras import backend as K
 from keras.models import Sequential
+
 from keras.layers.core import Flatten, Dense, Dropout
 from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD, RMSprop, Adam
@@ -17,38 +21,32 @@ from keras.optimizers import SGD, RMSprop, Adam
 def HANGnet(weights_path=None):
     model = Sequential()
     model.add(ZeroPadding2D((1, 1), input_shape=(180, 240, 1)))
-    model.add(Conv2D(12, (5, 5), activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(ZeroPadding2D((1, 1)))
 
-    model.add(Conv2D(12, (3, 3), activation='relu'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-    model.add(Dropout(0.5))
     model.add(ZeroPadding2D((1, 1)))
-
     model.add(Conv2D(16, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1, 1)))
 
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Conv2D(16, (3, 3), activation='relu'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-    model.add(Dropout(0.5))
-    model.add(ZeroPadding2D((1, 1)))
 
     model.add(Conv2D(32, (3, 3), activation='relu'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Dropout(0.5))
-
-    model.add(Conv2D(40, (3, 3), activation='relu'))
     model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-    model.add(Dropout(0.5))
+
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Conv2D(16, (3, 3), activation='relu'))
+
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Conv2D(16, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
 
     model.add(Flatten())
     #top layer of the VGG net
-    model.add(Dense(32, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(16, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(3, activation='linear'))
+    model.add(Dense(500, activation='elu'))
+    model.add(Dense(3, activation='elu'))
 
     if weights_path:
         model.load_weights(weights_path)
@@ -89,17 +87,19 @@ def main(training_data: 'npz file containing training data',
 
     # Scaling. As the input is a depth map, to apply scaling we need to unravel all the data
     # as if there was only one feature (depth)
+    logging.info("Scaling input features (X)...")
     scaler_X = StandardScaler()
     X_shape = X.shape
     X_1 = np.reshape(X, (X_shape[0], X_shape[1]*X_shape[2]))
     X_2 = np.reshape(X_1, (X_1.shape[0]*X_1.shape[1], 1))
+    X_scaled = np.where(X_2 >= 10, 10, X_2)  # Crop infinity to 10m (for data scaling)
     X_scaled = scaler_X.fit_transform(X_2)
     X_scaled_1 = np.reshape(X_scaled, X_1.shape)
     X_scaled_original_shape = np.reshape(X_scaled_1, X_shape)
     # store these off for predictions with unseen data
-    X_means = scaler_X.mean_
-    X_stds = scaler_X.scale_
-    np.savez('X_scaling.npz', X_means=X_means, X_stds=X_stds)
+    X_means = scaler_X.mean_  # Not now
+    X_stds = scaler_X.scale_  # Not now
+    np.savez('X_scaling.npz', X_means=X_means, X_stds=X_stds) # Not now
 
     if scale_output:
         scaler_Y = StandardScaler()
@@ -113,6 +113,7 @@ def main(training_data: 'npz file containing training data',
 
     # Train / test split
     X_scaled = X_scaled_original_shape[:, :, :, np.newaxis]
+    #X_scaled = X[:, :, :, np.newaxis]
     X_train, X_test, y_train, y_test = train_test_split(X_scaled, Y_scaled, test_size=0.2)
 
     # Test pretrained model
@@ -122,11 +123,13 @@ def main(training_data: 'npz file containing training data',
     if do_not_train:
         exit(0)
 
-    model.compile(loss="mse", optimizer=optimizer, metrics=["accuracy", "mse"])
+    model.compile(loss="mse", optimizer=optimizer, metrics=["mae", "mse"])
 
     history = model.fit(X_train, y_train, batch_size=batch_size, epochs=n_epoch,
-                        verbose=VERBOSE, validation_split=validation_split)
+                        verbose=VERBOSE, validation_data = (X_test, y_test) ) #, validation_split=validation_split)
     model.save_weights('hangnet-weights.h5')
+    with open('hangnet-history.pickle', 'wb') as f:
+        pickle.dump(history.history, f)
 
     score = model.evaluate(X_test, y_test, verbose=VERBOSE)
     logging.info("Test score: {}".format(score[0]))
@@ -134,14 +137,7 @@ def main(training_data: 'npz file containing training data',
 
     # list all data in history
     logging.info(history.history.keys())
-    # summarize history for accuracy
-    plt.plot(history.history['acc'])
-    plt.plot(history.history['val_acc'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
+
     # summarize history for loss
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
@@ -149,4 +145,4 @@ def main(training_data: 'npz file containing training data',
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
+    plt.savefig("loss_val_loss.png")
