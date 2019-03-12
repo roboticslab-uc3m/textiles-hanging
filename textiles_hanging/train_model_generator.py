28026e67da5ac7139a6aa5a0ae33819d61dfe69c
David-Estevez <david.estevez.fdez@gmail.com>
2019-03-12 17:52:38 +0100

    Add better logging to avoid erasing old files when testing

--- textiles_hanging/train_model_generator.py
+++ textiles_hanging/train_model_generator.py
@@ -29,6 +29,7 @@ batch_sizes = [32]
 @begin.logging
 def main(training_data_dir: 'folder containing training data',
          log_dir: 'TensorBoard logs go here'='./logs',
+         results_dir: 'Weights, train-validation partition and other results go here'='./results',
          do_not_train: 'If present, exits after summary'=False,
          n_epoch=20, batch_size=128, optimizer: 'Optimizer to be used [adam, sgd, rmsprop]'='adam',
          validation_split=0.2):
@@ -49,7 +50,6 @@ def main(training_data_dir: 'folder containing training data',
 
     INPUT_SHAPE = (240, 320, 1)  # This seems to be a reminder, as it is not used later
 
-
     # Get ids of files to load
     if os.path.exists(os.path.join(training_data_dir, 'dataset_files.pickle')):
         with open(os.path.join(training_data_dir, 'dataset_files.pickle'), 'rb') as f:
@@ -59,6 +59,7 @@ def main(training_data_dir: 'folder containing training data',
         with open('hangnet-last-dataset-filenames.pickle', 'wb') as f:
             pickle.dump(dataset_files, f)
 
+    # Split dataset
     np.random.shuffle(dataset_files)
     test_size = int(np.floor(0.2*len(dataset_files)))
     validation_size = int(np.floor(validation_split*(1-0.2)*len(dataset_files)))
@@ -68,20 +69,20 @@ def main(training_data_dir: 'folder containing training data',
     logging.info("Training set: {} examples".format(len(train_files)))
     logging.info("Validation set: {} examples".format(len(validation_files)))
     logging.info("Test set: {} examples".format(len(test_files)))
-    with open('hangnet-test-files.pickle', 'wb') as f:
+    t = time()
+    with open(os.path.join(results_dir, 'hangnet-test-files-{}.pickle'.format(t)), 'wb') as f:
         pickle.dump(test_files, f)
-    with open('hangnet-validation-files.pickle', 'wb') as f:
+    with open(os.path.join(results_dir, 'hangnet-validation-files-{}.pickle'.format(t)), 'wb') as f:
         pickle.dump(validation_files, f)
-    with open('hangnet-train-files.pickle', 'wb') as f:
+    with open(os.path.join(results_dir, 'hangnet-train-files-{}.pickle'.format(t)), 'wb') as f:
         pickle.dump(train_files, f)
 
-    # Create generator
+    # Create data generator
     params = {'batch_size': batch_size, 'resize': True,  'shuffle': True}
 
     training_generator = HangingDataGenerator(train_files, training_data_dir, **params)
     validation_generator = HangingDataGenerator(validation_files, training_data_dir, **params)
     test_generator = HangingDataGenerator(test_files, training_data_dir, **params)
-
 
     for model_name, model_generator in models_with_name:
         model = model_generator()
@@ -93,22 +94,43 @@ def main(training_data_dir: 'folder containing training data',
 
         for batch_size in batch_sizes:
             for opt_name, opt_generator in optimizers_with_name:
-                optimizer = opt_generator(lr=0.0001)
+                # Generate paths for logging to files (results/logs)
                 full_log_dir = os.path.abspath(os.path.expanduser(os.path.join(log_dir,
                                                                                "{}_{}_{}_{}".format(model_name,
                                                                                                     n_epoch,
                                                                                                     batch_size,
                                                                                                     opt_name))))
+                full_result_dir = os.path.join(results_dir, model_name)
+                weights_path = os.path.join(full_result_dir, 'hangnet-weights_{}_{}_{}_{}.h5'.format(model_name,
+                                                                                                     n_epoch,
+                                                                                                     batch_size,
+                                                                                                     opt_name))
+                history_path = os.path.join(full_result_dir, 'hangnet-history_{}_{}_{}_{}.pickle'.format(model_name,
+                                                                                                         n_epoch,
+                                                                                                         batch_size,
+                                                                                                         opt_name))
+                score_path = os.path.join(full_result_dir, 'hangnet-score_{}_{}_{}_{}.pickle'.format(model_name,
+                                                                                                     n_epoch,
+                                                                                                     batch_size,
+                                                                                                     opt_name))
+                figure_path = os.path.join(full_result_dir, "loss_plot_{}_{}_{}_{}.png".format(model_name, n_epoch,
+                                                                                               batch_size, opt_name))
+
+                # Create a tensorboard to log stats
                 tensorboard = TensorBoard(log_dir=full_log_dir, write_graph=False)
+
+                # Train model
+                optimizer = opt_generator(lr=0.0001)
                 model.compile(loss="mse", optimizer=optimizer, metrics=["mse", "mae"])
 
                 history = model.fit_generator(generator=training_generator, validation_data=validation_generator,
                                               use_multiprocessing=True, workers=6,
                                               epochs=n_epoch,
                                               verbose=VERBOSE, callbacks=[tensorboard])
-                model.save_weights('hangnet-weights_{}_{}_{}_{}.h5'.format(model_name, n_epoch, batch_size, opt_name))
-                with open('hangnet-history_{}_{}_{}_{}.pickle'.format(model_name, n_epoch, batch_size, opt_name),
-                          'wb') as f:
+
+                # Evaluate and save results to files
+                model.save_weights(weights_path)
+                with open( history_path, 'wb') as f:
                     pickle.dump(history.history, f)
 
                 score = model.evaluate_generator(generator=test_generator, verbose=VERBOSE,
@@ -117,8 +139,7 @@ def main(training_data_dir: 'folder containing training data',
                 for metric, s in zip(model.metrics_names, score):
                     logging.info("\t- {}: {}".format(metric, s))
 
-                with open('hangnet-score_{}_{}_{}_{}.pickle'.format(model_name, n_epoch, batch_size, opt_name),
-                          'wb') as f:
+                with open(score_path, 'wb') as f:
                     pickle.dump(score, f)
 
                 # Plot loss to a file for feedback
@@ -129,4 +150,4 @@ def main(training_data_dir: 'folder containing training data',
                 plt.ylabel('loss')
                 plt.xlabel('epoch')
                 plt.legend(['train', 'test'], loc='upper left')
-                plt.savefig("loss_val_loss_{}_{}_{}_{}.png".format(model_name, n_epoch, batch_size, opt_name))
+                plt.savefig(figure_path)
