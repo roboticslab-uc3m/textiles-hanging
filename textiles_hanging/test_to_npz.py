1310c63258d82fb38c83cf6160fa89aeaba39f5e
David-Estevez <david.estevez.fdez@gmail.com>
2019-05-31 16:41:40 +0200

    Add option to disable resizing

--- textiles_hanging/test_to_npz.py
+++ textiles_hanging/test_to_npz.py
@@ -9,7 +9,8 @@ from generators import HangingBinaryDataGenerator
 
 @begin.start(auto_convert=True)
 def main(test_files: 'Pickle file containing test set filenames',
-         input_folder: 'Folder containing the actual files'):
+         input_folder: 'Folder containing the actual files',
+         resize: 'Resize the input files (or not)'=True):
 
     test_files = os.path.abspath(os.path.expanduser(test_files))
     input_folder = os.path.abspath(os.path.expanduser(input_folder))
@@ -17,7 +18,9 @@ def main(test_files: 'Pickle file containing test set filenames',
     # Create data generator
     with open(test_files, 'rb') as f:
         test_files = pickle.load(f)
-    params = {'batch_size': 100, 'resize': True,  'shuffle': False}
+    params = {'batch_size': 100, 'resize': resize, 'shuffle': False}
+    if not resize:
+        params['dims']=(240, 320)
     training_generator = iter(HangingBinaryDataGenerator(test_files, input_folder, **params))
 
     X1, y1 = training_generator.__next__()
